a540cdc21baec044ada37541fbcf649c2ffe1b96
Diego Patrício <diegopatriciomd@gail.com>
2021-03-27 16:56:56 -0300

    Criando DTO e os Get/POSTMapping

--- backend/src/main/java/com/diegoptricio/bookstore/domain/Categoria.java
+++ backend/src/main/java/com/diegoptricio/bookstore/domain/Categoria.java
@@ -7,7 +7,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-@Data
+
 @Entity
 @Table(name = "Categorias")
 public class Categoria implements Serializable {
@@ -27,4 +27,35 @@ public class Categoria implements Serializable {
     private List<Livro> livros = new ArrayList<>();
 
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public List<Livro> getLivros() {
+        return livros;
+    }
+
+    public void setLivros(List<Livro> livros) {
+        this.livros = livros;
+    }
 }
